3028037b31ba56519065093586f0cc83f306ce96
xyred <felixherold@gmx.de>
2026-02-23 13:50:20 +0100

    feat(tests): add transactional support to UserRepositoryTest for improved test isolation

--- src/test/java/de/fherold/skillswap/repository/UserRepositoryTest.java
+++ src/test/java/de/fherold/skillswap/repository/UserRepositoryTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Transactional
 class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
