ef481f71623fe4c0fc3ff6ed54282f692546d139
xyred <felixherold@gmx.de>
2026-02-23 14:27:16 +0100

    feat(model): enhance User entity with Lombok annotations for cleaner code and improved equality/hashcode handling

--- src/main/java/de/fherold/skillswap/model/User.java
+++ src/main/java/de/fherold/skillswap/model/User.java
@@ -4,22 +4,24 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "skills")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @NotBlank(message = "Username is required")
