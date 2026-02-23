31e69044532d5e66b37dd194c0fdeb4563153502
xyred <felixherold@gmx.de>
2026-02-23 15:07:56 +0100

    feat(exception): add BusinessRuleException for domain logic violations

---
+++ src/main/java/de/fherold/skillswap/exception/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package de.fherold.skillswap.exception;
+
+public class BusinessRuleException extends RuntimeException {
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
